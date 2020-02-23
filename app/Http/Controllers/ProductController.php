000b0c7e176e01a4c4d3c54842d1992aea4a03b2
jona <sambranojonathan@gmail.com>
2020-02-23 10:07:56 -0400

    Add product controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Libraries\Machine;
+use App\Product;
+
+class ProductController extends Controller
+{
+
+    public function getProducts()
+    {
+        return Machine::statusMachine();
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
